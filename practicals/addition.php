9021288b6903a71c630b6f252df5d05a83426dee
nikunjchhatani <nikunjchhatani@gmail.com>
2023-11-04 14:43:56 +0530

    First commit

---
+++ practicals/addition.php
@@ -0,0 +1,9 @@
+<?php
+$number1 = 5; // Replace with your first number
+$number2 = 7; // Replace with your second number
+
+$sum = $number1 + $number2;
+
+echo "The sum of $number1 and $number2 is: $sum";
+?>
+
