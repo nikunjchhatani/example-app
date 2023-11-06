4e8c78cabfcac5e767dc3a6d768cd458c4259379
nikunjchhatani <nikunjchhatani@gmail.com>
2023-11-06 22:15:04 +0530

    Add Swap Numbers Code

---
+++ practicals/swap.php
@@ -0,0 +1,17 @@
+<?php
+// Define two numbers
+$num1 = 10;
+$num2 = 20;
+
+// Display the original values
+echo "Original values: num1 = $num1, num2 = $num2<br>";
+
+// Swap the numbers using a temporary variable
+$temp = $num1;
+$num1 = $num2;
+$num2 = $temp;
+
+// Display the swapped values
+echo "Swapped values: num1 = $num1, num2 = $num2";
+?>
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Route::get('/veggies/baigan', function () {
+    return 'Baigan';
+});*/
+
+Route::get('/veggies/{veggiename}', function (string $veggiename) {
+	return $veggiename;
+})->whereIn('veggiename', ['baigan', 'bhindi', 'aaloo','gobhi']);
