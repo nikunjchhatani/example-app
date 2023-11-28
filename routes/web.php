78c9d76bdc6a2fa4ecee4ff0571beb5d26742707
nikunjchhatani <nikunjchhatani@gmail.com>
2023-11-28 09:53:31 +0530

    Add new playlist page

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,7 @@ Route::get('/songs_static', function () {
 Route::get('/songs', function () {
     return view('songs', [ 'songs' => Song::all() ] );
 });
+
+Route::get('/playlists/{playlistId}', function (string $playlistId) {
+    return view('playlist', ['songs' => Song::all(), 'playlistId' => $playlistId ]);
+  });
